e3af22235431b20886855755a092300851eb62cf
Adliano Alves <adliano@me.com>
2019-08-21 22:46:27 -0700

    fixed bobble up css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,12 @@ import './style.css'
 
 export function Home() {
   return (
-    <>
+    <div className="bgImage">
       <Container className='my-3'>
         <Jumbotron fluid className='text-light'>
           <h1 className="text-center header">Let's Build Something</h1>
         </Jumbotron>
       </Container>
-    </>
+    </div>
   )
 }
