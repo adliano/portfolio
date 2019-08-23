c5d729b535d464b9313bb04c946b4b683946c9c5
Adliano Alves <adliano@me.com>
2019-08-22 22:25:44 -0700

    added animation on home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Container, Jumbotron } from 'react-bootstrap'
+import { Zoom } from 'react-reveal'
 import './style.css'
 
-export function Home() {
+export function Home () {
   return (
-    <div className="bgImage">
-      <Container className='my-3'>
-        <Jumbotron fluid className='text-light'>
-          <h1 className="text-center header">Let's Build Something</h1>
-        </Jumbotron>
-      </Container>
-    </div>
+    <>
+      <Zoom>
+        <div className='bgImage'>
+          <Container className='my-3'>
+            <Jumbotron fluid className='text-light'>
+              <h1 className='text-center header'>
+                <Zoom right cascade>
+                  Let's Build Something
+                </Zoom>
+              </h1>
+            </Jumbotron>
+            {/* <h1>&lt;/&gt;</h1> */}
+          </Container>
+        </div>
+      </Zoom>
+    </>
   )
 }
