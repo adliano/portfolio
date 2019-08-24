7479ae298737ef3181bcbcc510d093d0669db301
Adliano Alves <adliano@me.com>
2019-08-24 00:45:44 -0700

    working o media query

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -14,15 +14,37 @@ export function About () {
           <Fade duration={500}>
             <Image src='/assets/images/screen_about.png' fluid />
           </Fade>
-          <div className='text-white greeting bg-dark px-3'>
+          <div className='text-white greeting bg-dark px-3 pt-3'>
             <Typing startDelay={1000} speed={5}>
               <h3>
                 Hi I'm Adriano.
                 <br />
-                <Typing.Delay ms={1000} />A full-stack web developer, computer
+                <Typing.Delay ms={1500} />A full-stack web developer, computer
                 scientist and a mathematics lover.
               </h3>
             </Typing>
+            <div>
+              <Typing startDelay={7500} speed={5}>
+                <h4>
+                  I’m excited to start my career in web development, Looking
+                  forward to starting using all t he knowledge I gained during
+                  these years at this highway to shell.
+                </h4>
+                <h4>
+                  I'm certified Android Developer from City College of San
+                  Francisco with strong knowledge in Java SE, Jave EE, MySQL
+                  Database Administration, Oracle 12C database Administration,
+                  Linux System Administration, Linux Network
+                  Administration,Programming Techniques for XML, Dart, Swift and
+                  some C++. Current Learning Web development FullStack at
+                  Berkeley Bootcamp. I am Unix command line lover and strong
+                  believe that everything it's possible until you stop believing
+                  that you can do it. I love to learn new things, ride, travel
+                  and drink coffee. I also l ove to help debug problems, because
+                  thats the way i learn more.
+                </h4>
+              </Typing>
+            </div>
           </div>
         </div>
       </Container>
