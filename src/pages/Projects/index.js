c3ea5c39360e8dd00c5f10efeb16c24b3436bdf1
Adliano Alves <adliano@me.com>
2019-08-25 03:28:10 -0700

    added 2 cards

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,13 +1,46 @@
 import React from 'react'
-import { Container, Jumbotron } from 'react-bootstrap'
+import { Card, CardColumns, Container, Jumbotron } from 'react-bootstrap'
+import { Bounce, Zoom } from 'react-reveal'
 
-export function Projects() {
+export function Projects () {
   return (
     <>
+      <Jumbotron className='bg-transparent text-light' fluid>
+        <h1 className='text-center'>List of Projects</h1>
+      </Jumbotron>
       <Container className='my-3'>
-        <Jumbotron fluid>
-          <h1 className="text-center">List of Projects</h1>
-        </Jumbotron>
+        <CardColumns>
+          <Bounce top cascade>
+            {/* ************************************* */}
+            {/* ************** 90's Game ************ */}
+            {/* ************************************* */}
+            <Card>
+              <Card.Img variant='top' src='/assets/images/90sgame.gif' />
+              <Card.Body>
+                <Card.Title>90's Music Word Guess</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            {/* ************************************* */}
+            {/* ************** RPG Game ************* */}
+            {/* ************************************* */}
+            <Card>
+              <Card.Img variant='top' src='/assets/images/rpg.gif' />
+              <Card.Body>
+                <Card.Title>RPG Game</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Bounce>
+        </CardColumns>
       </Container>
     </>
   )
