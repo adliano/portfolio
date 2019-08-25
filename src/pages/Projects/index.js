3f45dc257e381f0a33ad3fbbe0019af85bb6e8f0
Adliano Alves <adliano@me.com>
2019-08-25 13:20:48 -0700

    working on cards with projects

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -8,8 +8,8 @@ export function Projects () {
       <Jumbotron className='bg-transparent text-light' fluid>
         <h1 className='text-center'>List of Projects</h1>
       </Jumbotron>
-      <Container className='my-3'>
-        <CardColumns>
+      <Container className='px-5 mx-auto' >
+        <CardColumns className='mx-auto px-5 text-center' >
           <Bounce top cascade>
             {/* ************************************* */}
             {/* ************** 90's Game ************ */}
@@ -32,6 +32,76 @@ export function Projects () {
               <Card.Img variant='top' src='/assets/images/rpg.gif' />
               <Card.Body>
                 <Card.Title>RPG Game</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            {/* ************************************* */}
+            {/* ************ Smart Trivia *********** */}
+            {/* ************************************* */}
+            <Card className='bg-secondary text-light'>
+              <Card.Img className='p-1 bg-light' variant='top' src='/assets/images/smart_trivia.gif' />
+              <Card.Body>
+                <Card.Title className='bg-white text-dark'>Trivia Smart</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            {/* ************************************* */}
+            {/* ************* Giftastic ************* */}
+            {/* ************************************* */}
+            <Card>
+              <Card.Img variant='top' src='http://www.alves.tech/assets/images/giftastic.jpg' />
+              <Card.Body>
+                <Card.Title>Giftastic</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            {/* ************************************* */}
+            {/* ************* Bamazon ************* */}
+            {/* ************************************* */}
+            <Card>
+              <Card.Img variant='top' src='http://www.alves.tech/assets/images/bamazon.gif' />
+              <Card.Body>
+                <Card.Title>Bamazon</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            {/* ************************************* */}
+            {/* *********** Friend Finder *********** */}
+            {/* ************************************* */}
+            <Card>
+              <Card.Img variant='top' src='http://www.alves.tech/assets/images/friendFinder.png' />
+              <Card.Body>
+                <Card.Title>Friend Finder</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            {/* ************************************* */}
+            {/* ************** Petinder ************* */}
+            {/* ************************************* */}
+            <Card>
+              <Card.Img variant='top' src='http://www.alves.tech/assets/images/petinder.png' />
+              <Card.Body>
+                <Card.Title>Petinder</Card.Title>
                 <Card.Text>
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
