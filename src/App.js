a87b9c69aa17851f5765a8de23a1ce7b06ee9c75
Adliano Alves <adliano@me.com>
2019-08-24 19:11:38 -0700

    Add icons follow me icons to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { MyNavbar } from './components'
 import { About, Home, Projects } from './pages'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import './App.css';
+import { SocialIcon } from 'react-social-icons'
+
+import './App.css'
 // Library used for icons
 library.add(fab, fas)
 
-function App() {
+function App () {
   return (
     <>
+      {/* ************************** */}
+      <div className='follow-me'>
+        <ul>
+          <li>
+            <SocialIcon url='https://github.com/adliano' bgColor='#444' fgColor='fff'/>
+          </li>
+          <li>
+            <SocialIcon url='https://www.linkedin.com/in/adliano/' />
+          </li>
+          <li>
+            <SocialIcon url='https://www.instagram.com/adliano/' />
+          </li>
+        </ul>
+      </div>
+      {/* ************************** */}
       <BrowserRouter>
         <MyNavbar />
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/about' component={About}/>
-        <Route exact path='/projects' component={Projects}/>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/projects' component={Projects} />
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Container, Image } from 'react-bootstrap'
 import { Fade } from 'react-reveal'
 import Typing from 'react-typing-animation'
-// import { Cursor } from '../../components'
 
 import './style.css'
 
@@ -16,33 +15,33 @@ export function About () {
           </Fade>
           <div className='text-white greeting bg-dark px-3 pt-3'>
             <Typing startDelay={1000} speed={5}>
-              <h3>
+              <h4>
                 Hi I'm Adriano.
                 <br />
                 <Typing.Delay ms={1500} />A full-stack web developer, computer
                 scientist and a mathematics lover.
-              </h3>
+              </h4>
             </Typing>
             <div>
               <Typing startDelay={7500} speed={5}>
-                <h4>
+                <h6>
                   I’m excited to start my career in web development, Looking
                   forward to starting using all t he knowledge I gained during
-                  these years at this highway to shell.
-                </h4>
-                <h4>
-                  I'm certified Android Developer from City College of San
-                  Francisco with strong knowledge in Java SE, Jave EE, MySQL
-                  Database Administration, Oracle 12C database Administration,
-                  Linux System Administration, Linux Network
-                  Administration,Programming Techniques for XML, Dart, Swift and
-                  some C++. Current Learning Web development FullStack at
-                  Berkeley Bootcamp. I am Unix command line lover and strong
-                  believe that everything it's possible until you stop believing
-                  that you can do it. I love to learn new things, ride, travel
-                  and drink coffee. I also l ove to help debug problems, because
-                  thats the way i learn more.
-                </h4>
+                  these years.
+                </h6>
+                <h6>
+                  I'm certified in Android development at City College of San
+                  Francisco and Web development FullStack at Berkeley with
+                  strong knowledge in Java SE, Jave EE, MySQL Database
+                  Administration, Oracle 12C database Administration, Linux
+                  System Administration, Linux Network Administration,
+                  Programming Techniques for XML, Dart, Swift and some C++. I am
+                  an Unix command line lover and strong believe that everything
+                  it's possible until you stop believing that you can do it. I
+                  love to learn new things, ride, travel and drink coffee. I
+                  also love to help debug problems, because thats the way i
+                  learn more.
+                </h6>
               </Typing>
             </div>
           </div>
